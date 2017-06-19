d581b2f6fc4196f51c6682e54cab5f04e7d8709c
Dinhmanh96 <Dinhmanh96@gmail.com>
2017-06-19 10:59:01 +0700

    Edit Scss

--- Admin/main.php
+++ Admin/main.php
@@ -17,7 +17,7 @@
 		    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Lato">
 		    <script type="text/javascript" src="../js/jquery-3.2.1.min.js"></script>
 		    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
-		    <link rel="stylesheet" type="text/css" href="../css/strict-admin.css">
+		    <link rel="stylesheet" type="text/css" href="../css/strict-admin1.css">
 		</head>
 		<body>
 			<nav class="navbar">
